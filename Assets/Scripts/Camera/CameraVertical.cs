3fb28d4c0f46bd5c105e04c83afa2e669d1ce5ae
Sean Mauff <sjmauff@hotmail.com>
2016-07-18 10:38:41 +1000

    moved everything up one level to match the main project before merge

---
+++ Assets/Scripts/Camera/CameraVertical.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraVertical : MonoBehaviour {
+    string playerNumber;
+	// Use this for initialization
+	void Start () {
+        playerNumber = GetComponentInParent<CameraHorizontal>().playerNumber;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float v = Input.GetAxis(this.playerNumber + " Camera Vertical");
+        this.transform.Rotate(new Vector3(v * 2, 0, 0));
+    }
+}
