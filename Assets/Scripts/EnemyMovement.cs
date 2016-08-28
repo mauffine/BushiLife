22e9620e81058d981dc52087dfa3b7ecba1eaf42
Nameless Student <nathan.w.blair@gmail.com>
2016-08-29 09:52:19 +1000

    updated ai

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,15 +4,28 @@ using System.Collections.Generic;
 
 public class EnemyMovement : MonoBehaviour {
 	NavMeshAgent agent;
+    AIController ai;
+    public float maxLeadDist;
 
 	void Start()
 	{
-		this.agent = GetComponent<NavMeshAgent>();
 	}
+
+    public void Init(AIController ai)
+    {
+        this.agent = GetComponent<NavMeshAgent>();
+        this.ai = ai;
+    }
 
 	void Update()
 	{
 		this.UpdateDestination();
+
+        this.ai.target = this.transform.position;
+        if ((this.ai.transform.position - this.transform.position).magnitude > this.maxLeadDist)
+        {
+            Teleport(this.ai.transform.position);
+        }
 	}
 
 	void UpdateDestination()
@@ -22,4 +35,11 @@ public class EnemyMovement : MonoBehaviour {
 
 		this.agent.SetDestination(players[0].transform.position);
 	}
+
+    void Teleport(Vector3 position)
+    {
+        this.agent.enabled = false;
+        this.transform.position = position;
+        this.agent.enabled = true;
+    }
 }
