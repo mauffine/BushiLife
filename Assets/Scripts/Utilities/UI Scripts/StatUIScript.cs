8309bc5d34f16d32763098e42c75aa3e83920b78
Sean Mauff <sjmauff@hotmail.com>
2016-09-14 12:38:09 +1000

    +Removed Phyisical health bars on the players +UI bars implemented +added daze animation after the block is broken

--- Assets/Scripts/Utilities/UI Scripts/StatUIScript.cs
+++ Assets/Scripts/Utilities/UI Scripts/StatUIScript.cs
@@ -8,52 +8,96 @@ enum statType
     Experience,
     Level
 }
+enum UIType
+{
+    Bar,
+    Text
+}
 public class StatUIScript : MonoBehaviour {
     [SerializeField]
     Character character;
     [SerializeField]
     statType Stat = statType.Health;
+    UIType myType;
     float max = 100;
     float min = 0;
     float current = 100;
+
 	// Use this for initialization
 	void Start () {
+        if (gameObject.GetComponent<Text>() != null)
+            this.myType = UIType.Text;
+        else
+            this.myType = UIType.Bar;
 	}
-	
 	// Update is called once per frame
 	void Update () {
-        switch (this.Stat)
+        if (this.myType == UIType.Text)
         {
-            case (statType.Health):
-                {
-                    this.current = this.character.stats.health.val;
-                    gameObject.GetComponent<Text>().text = "health:" + this.max.ToString() + "/" + this.current.ToString();
+            switch (this.Stat)
+            {
+                case (statType.Health):
+                    {
+                        this.current = this.character.stats.health.val;
+                        gameObject.GetComponent<Text>().text = "health:" + this.max.ToString() + "/" + this.current.ToString();
+                        break;
+                    }
+                case (statType.Stamina):
+                    {
+                        this.current = this.character.stats.stamina.val;
+                        gameObject.GetComponent<Text>().text = "Stamina:" + this.max.ToString() + "/" + this.current.ToString();
+                        break;
+                    }
+                case (statType.Level):
+                    {
+                        this.current = this.character.stats.level.val;
+                        gameObject.GetComponent<Text>().text = "Level:" + this.current.ToString();
+                        break;
+                    }
+                case (statType.Experience):
+                    {
+                        this.current = this.character.stats.experience.val;
+                        gameObject.GetComponent<Text>().text = "Exp:" + this.max.ToString() + "/" + this.current.ToString();
+                        break;
+                    }
+                default:
                     break;
-                }
-            case (statType.Stamina):
-                {
-                    this.current = this.character.stats.stamina.val;
-                    gameObject.GetComponent<Text>().text = "Stamina:" + this.max.ToString() + "/" + this.current.ToString();
+            }
+        }
+        else
+        {
+            switch (this.Stat)
+            {
+                case (statType.Health):
+                    {
+                        this.current = this.character.stats.health.val;
+                        GetComponent<Image>().fillAmount = current / max;
+                        break;
+                    }
+                case (statType.Stamina):
+                    {
+                        this.current = this.character.stats.stamina.val;
+                        GetComponent<Image>().fillAmount = current / max;
+                        break;
+                    }
+                case (statType.Level):
+                    {
+                        this.current = this.character.stats.level.val;
+                        GetComponent<Image>().fillAmount = current / max;
+                        break;
+                    }
+                case (statType.Experience):
+                    {
+                        this.current = this.character.stats.experience.val;
+                        GetComponent<Image>().fillAmount = current / max;
+                        break;
+                    }
+                default:
                     break;
-                }
-            case (statType.Level):
-                {
-                    this.current = this.character.stats.level.val;
-                    gameObject.GetComponent<Text>().text = "Level:" + this.current.ToString();
-                    break;
-                }
-            case (statType.Experience):
-                {
-                    this.current = this.character.stats.experience.val;
-                    gameObject.GetComponent<Text>().text = "Exp:" + this.max.ToString() + "/" + this.current.ToString();
-                    break;
-                }
-            default:
-                break;
+            }
         }
        
 	}
-
     public void SetCharacter(GameObject _player)
     {
         this.character = _player.GetComponent<Character>();
