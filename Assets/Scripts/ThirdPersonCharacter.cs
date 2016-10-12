4f9f64b8bec1c7623914d3313635886bf4a81dd7
Sean Mauff <sjmauff@hotmail.com>
2016-10-12 12:04:38 +1100

    FIxed skypound bug

--- Assets/Scripts/ThirdPersonCharacter.cs
+++ Assets/Scripts/ThirdPersonCharacter.cs
@@ -298,7 +298,7 @@ public class ThirdPersonCharacter : MonoBehaviour
         RaycastHit hitInfo;
         // 0.1f is a small offset to start the ray from inside the character
         // it is also good to note that the transform position in the sample assets is at the base of the character
-        if (Physics.Raycast(this.transform.position + (Vector3.up * 0.1f), Vector3.down, out hitInfo, this.m_GroundCheckDistance))
+        if (Physics.Raycast(this.transform.position + (Vector3.up * 0.1f), Vector3.down, out hitInfo, this.m_GroundCheckDistance) && !hitInfo.collider.isTrigger)
         {
             this.m_GroundNormal = hitInfo.normal;
             this.m_IsGrounded = true;
