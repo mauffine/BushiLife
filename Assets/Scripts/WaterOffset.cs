bb98624978d0a5f46687ae3fe2c62c5bdaf303ae
Jordan Williams <jrwilliams.design@gmail.com>
2016-11-02 14:13:03 +1100

    All needed water and river assets (missing waterfall top ripple) WIP

---
+++ Assets/Scripts/WaterOffset.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterOffset : MonoBehaviour {
+
+	public float speedX = 0.1f;
+	public float speedY = 0.1f;
+	private float curX;
+	private float curY;
+
+
+	void Awake () 
+	{
+		//curX = GetComponent<Renderer>().material.mainTextureOffset.x;
+		curX = GetComponent<Renderer>().material.mainTextureOffset.y;
+	}		
+	
+	
+	void Update () 
+	{
+		//curX += Time.deltaTime * speedX;
+		curY += Time.deltaTime * speedY;
+		GetComponent<Renderer>().material.SetTextureOffset ("_MainTex", new Vector2(curX, curY));
+	}
+}
